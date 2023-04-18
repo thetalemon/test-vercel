a7698cdb913ecb4b14dc170d85e440499de17814
thetalemon <sasami0314@gmail.com>
2023-04-18 14:02:46 +0900

    test

--- middleware.ts
+++ middleware.ts
@@ -1,22 +1,10 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const config = {
-  matcher: ['/:path*'],
+  matcher: ['/', '/index'],
 }
 
 export function middleware(req: NextRequest) {
-  if (process.env.NODE_ENV === 'development') {
-    return NextResponse.next()
-  }
-
-  if (req.method === 'POST' && req.nextUrl.pathname === '/api/revalidate') {
-    return NextResponse.next()
-  }
-
-  if (process.env.ENABLE_BASIC_AUTH !== 'true') {
-    return NextResponse.next()
-  }
-
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
 
@@ -24,14 +12,11 @@ export function middleware(req: NextRequest) {
     const authValue = basicAuth.split(' ')[1]
     const [user, pwd] = atob(authValue).split(':')
 
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      pwd === process.env.BASIC_AUTH_PASSWORD
-    ) {
+    if (user === '4dmin' && pwd === 'testpwd123') {
       return NextResponse.next()
     }
   }
-  url.pathname = '/api/basic-auth'
+  url.pathname = '/api/auth'
 
   return NextResponse.rewrite(url)
 }
