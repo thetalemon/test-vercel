19f1b059bda7beeaee22144cedc736cbdd2c336f
thetalemon <sasami0314@gmail.com>
2023-04-18 14:44:37 +0900

    middleware削除してみる

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,20 @@
-import { NextRequest, NextResponse } from 'next/server'
+// import { NextRequest, NextResponse } from 'next/server'
 
-export function middleware(req: NextRequest) {
-  const basicAuth = req.headers.get('authorization')
-  const url = req.nextUrl
+// export function middleware(req: NextRequest) {
+//   const basicAuth = req.headers.get('authorization')
+//   const url = req.nextUrl
 
-  if (process.env.NODE_ENV === 'production') {
-    if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1]
-      const [user, pwd] = atob(authValue).split(':')
+//   if (process.env.NODE_ENV === 'production') {
+//     if (basicAuth) {
+//       const authValue = basicAuth.split(' ')[1]
+//       const [user, pwd] = atob(authValue).split(':')
 
-      if (user === '4dmin' && pwd === 'testpwd123') {
-        return NextResponse.next()
-      }
-    }
-    url.pathname = '/api/auth'
+//       if (user === '4dmin' && pwd === 'testpwd123') {
+//         return NextResponse.next()
+//       }
+//     }
+//     url.pathname = '/api/auth'
 
-    return NextResponse.rewrite(url)
-  }
-}
+//     return NextResponse.rewrite(url)
+//   }
+// }
