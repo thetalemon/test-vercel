32fb972bf18273beb52c9c1d4a65e74cbb59758a
thetalemon <sasami0314@gmail.com>
2023-04-18 15:09:29 +0900

    これでいい感じに動くようになるはず

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,37 @@
 import { NextRequest, NextResponse } from 'next/server'
 
+export const config = {
+  matcher: ['/:path*'],
+}
+
 export function middleware(req: NextRequest) {
+  if (process.env.NODE_ENV === 'development') {
+    return NextResponse.next()
+  }
+
+  if (req.method === 'POST' && req.nextUrl.pathname === '/api/revalidate') {
+    return NextResponse.next()
+  }
+
+  if (process.env.ENABLE_BASIC_AUTH !== 'true') {
+    return NextResponse.next()
+  }
+
   const basicAuth = req.headers.get('authorization')
   const url = req.nextUrl
-  if (process.env.NODE_ENV === 'production') {
-    if (basicAuth) {
-      const authValue = basicAuth.split(' ')[1]
-      const [user, pwd] = atob(authValue).split(':')
-      if (user === '4dmin' && pwd === 'testpwd123') {
-        return NextResponse.next()
-      }
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (
+      user === process.env.BASIC_AUTH_USER &&
+      pwd === process.env.BASIC_AUTH_PASSWORD
+    ) {
+      return NextResponse.next()
     }
-    url.pathname = '/api/auth'
-    return NextResponse.rewrite(url)
   }
+  url.pathname = '/api/basic-auth'
+
+  return NextResponse.rewrite(url)
 }
